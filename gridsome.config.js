d0a6aecb878a39d6c55b2af81fc5e988ce0d3f2b
Sohaib Sherif <s.alsharif@tech.arttech.ly>
2020-07-21 01:00:13 +0200

    initial commit

---
+++ gridsome.config.js
@@ -0,0 +1,52 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here requires a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: `Sohaib Sherif`,
+  siteDescription: `A simple & minimalistic blog where I share my knowledge and all the experiences and diffculties
+  that I faced as a Software Developer. I also write about life, philosophy and anything in between`,
+
+  templates: {
+    Post: '/:title',
+    Tag: '/tag/:id'
+  },
+
+  plugins: [
+    {
+      // Create posts from markdown files
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Post',
+        path: 'content/posts/*.md',
+        refs: {
+          // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
+          tags: {
+            typeName: 'Tag',
+            create: true
+          }
+        }
+      }
+    },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`
+      }
+    },
+  ],
+
+  transformers: {
+    //Add markdown support to all file-system sources
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        '@gridsome/remark-prismjs'
+      ]
+    }
+  }
+}
